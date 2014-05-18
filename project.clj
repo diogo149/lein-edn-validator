6cc6929d87764477a3bf782186f40081cae80e74
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-05-17 17:30:14 -0700

    make snapshot release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edn-validator "0.1.0"
+(defproject edn-validator "0.2.0-SNAPSHOT"
   :description
   "An edn validator that isolates errors to the smallest possible form."
   :url "https://github.com/diogo149/lein-edn-validator"
